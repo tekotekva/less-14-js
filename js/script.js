9c4264681d6afaf5dbb8c6d5e8c9fc328392474b
tekvatekva <165680892+tekvatekva@users.noreply.github.com>
2024-06-30 22:52:53 +0400

    task 2

--- js/script.js
+++ js/script.js
@@ -1,12 +1,21 @@
+// task #1
 
 let number = 100;
 
 if (number <50) {
     console.log('naklebia 50')
 }
-if (number >20) {
+else if (number >20) {
     console.log('metia 20')
 }
 else {
     console.log('error')
 }
+
+
+// task #2
+
+let conditionResult = name1 = 'mariam' ? 'true' : 'error';
+console.log(conditionResult);
+
+//  task #3
