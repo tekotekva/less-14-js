97130198d33894d54434265b22859c5367707135
tekvatekva <165680892+tekvatekva@users.noreply.github.com>
2024-07-01 17:49:06 +0400

    II

--- js/script.js
+++ js/script.js
@@ -19,3 +19,70 @@ let conditionResult = name1 = 'mariam' ? 'true' : 'error';
 console.log(conditionResult);
 
 //  task #3
+
+
+// task #4
+
+let array1= [1, 2, 4, -1, -3, 8, 7, 0, -5, 18, 12];
+
+for (let i = 1; i < 10; i++) {
+    console.log(array1[i]);   
+}
+
+// task #6
+
+// This is old vertion
+
+let user2 = {
+    name: 'anna',
+    age:  20,
+    studentstatus: 'active'
+}
+ if (user2.age < 18 && user2.studentstatus == 'active') {
+    console.log('hello user');
+ }
+ else if (user2.name == 'levan') {
+    console.log('hello levani');
+ }
+ else if (user2.studentstatus == 'active' || user2.age < 25 ) {
+    console.log('hello anna'); 
+ }
+ else {
+    console.log('error');
+ }
+
+// This is new vertion (ternary operator)
+
+let user2 = {
+    name: 'anna',
+    age:  20,
+    studentstatus: 'active'
+}
+
+let conditionResult =
+    user2.age < 18 && user2.studentstatus == 'active'
+    ? 'hello user'
+    :user2.name == 'levan'
+    ? 'hello levan'
+    :user2.studentstatus == 'active' || user2.age <25
+    ? 'hello anna'
+    :'error'
+console.log(conditionResult);
+
+
+    
+
+
+
+// task #7
+let array6 = [ 2, 3, 5, 10,25,24,11, 100 , 6,7, 10 ]
+console.log(array6[]);
+
+// task #8
+
+let users = [ 
+    {username: 'giorgi', status: false},
+    {username: 'levani', status: false},
+    {username: 'anna', status: true}
+]
+console.log(users[2].status)
